7c2c8851993883ebaaff50fae6f5a0fcfd360d92
Mohammad Hj Hassan <35444902+MohammadHHassan@users.noreply.github.com>
2021-07-20 15:39:25 +0800

    Add files via upload

---
+++ day1/HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>		// Header file for input & output
+
+int main(void)			// Main function
+{
+	printf("Hello World!");		// Prints out "Hello World!"
+	
+	return 0;
+}
---
+++ day1/LearningNewLines.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World!\nGoodbye World :(");
+	//printf("Goodbye World :(");
+	
+	return 0;
+}
