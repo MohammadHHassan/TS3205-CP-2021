53d10ab8c4fe379eecc37bd4eab3944bac230c2b
Mohammad Hj Hassan <35444902+MohammadHHassan@users.noreply.github.com>
2021-09-29 13:34:20 +0800

    Add files via upload

---
+++ java_day1/HelloWorld.java
@@ -0,0 +1,12 @@
+package java_day1;
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
+}
